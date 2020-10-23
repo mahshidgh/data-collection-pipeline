99c6edc656a3b999223132b470ee95abe497abe2
speeeday <sanjayc@andrew.cmu.edu>
2020-10-23 14:48:00 -0700

    Update upload.sh

--- columbia/upload.sh
+++ columbia/upload.sh
@@ -5,22 +5,17 @@ source constants.sh
 while true; do
     for f in $1/uploads/*
     do
-	FILESIZE=$(stat -c%s "$f")
-    
-	if [ $FILESIZE -ge $MAX_FILESIZE ]; then
-
 	    # run offline anonymization on this file
 	    echo "Attempting to anonymize $f"
 	    ./anonymize-offline.sh $f
-	
+
 	    # TODO: upload this file [$f-anonymized] to google drive
 
 	    echo "Attempting to upload $f-anonymized"	
 	    gupload $f-anonymized -c columbia
-	
+
 	    rm -f $f
 	    rm -f $f-anonymized
-	fi
     done
     sleep 10
 done
